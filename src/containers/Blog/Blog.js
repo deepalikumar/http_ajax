a03d8b56ad6b86c01882e8a436005550440a0d37
deepaliprasad <deepaliprasad123@gmail.com>
2018-01-02 15:16:00 +0530

    dynamic routing and switch property of the router

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import Posts from './Posts/Posts';
+import FullPost from './FullPost/FullPost';
 import NewPost from './NewPost/NewPost';
-import { Route, NavLink } from 'react-router-dom';
+import { Route, NavLink, Switch } from 'react-router-dom';
 import './Blog.css';
 
 class Blog extends Component {
@@ -32,7 +33,10 @@ class Blog extends Component {
                     {/* <Route path="/" exact render={() => <h1> Home </h1> }/>
                     <Route path="/" render={() => <h1> Home again </h1> }/>  */}
                     <Route path="/" exact component={Posts} />
-                    <Route path="/new-post" component={ NewPost } />
+                    <Switch>
+                        <Route path="/new-post" component={ NewPost } />
+                        <Route path="/:id" exact component={FullPost} />
+                    </Switch>
 
                 </div>
                 
