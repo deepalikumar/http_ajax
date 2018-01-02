98b54653b20c0864076d4cd537ec2946c92009f8
deepaliprasad <deepaliprasad123@gmail.com>
2018-01-02 16:03:30 +0530

    nested routing

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import axios from '../../../axios';
 import Post from '../../../components/Post/Post';
 import './Posts.css';
-import { Link } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
+import FullPost from '../FullPost/FullPost';
 
 
 
@@ -31,7 +32,7 @@ class Posts extends Component {
 
     postsSelectedHandler = (id) => {
        // this.setState({selectedPostId: id})
-       this.props.history.push('/' + id);
+       this.props.history.push('/posts/' + id);
     }
 
     posts
@@ -40,7 +41,7 @@ class Posts extends Component {
         if(!this.state.error){
             posts = this.state.posts.map (post => {
                 return (
-                   //<Link to={'/' + post.id} key={post.id}> 
+                   //<Link to={'/posts/' + post.id} key={post.id}> 
                         <Post 
                             key={post.id}
                             title={post.title} 
@@ -51,9 +52,12 @@ class Posts extends Component {
             })
         } 
         return (
-            <section className="Posts">
-                { posts }
-            </section>
+            <div>
+                <section className="Posts">
+                    { posts }
+                </section>
+                <Route path={this.props.match.url + '/:id'} exact component={FullPost} />
+            </div>
         )
     }
 
