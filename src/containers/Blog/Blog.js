32e00015834b39ad8abc2c8cd512b56c54de2b1a
deepaliprasad <deepaliprasad123@gmail.com>
2018-01-03 13:11:21 +0530

    lazily loading the path whenever its required using asyncomponent

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,13 +2,18 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import Posts from './Posts/Posts';
 // import FullPost from './FullPost/FullPost';
-import NewPost from './NewPost/NewPost';
+//import NewPost from './NewPost/NewPost';
+import asyncComponent from '../../hoc/asyncComponent';
 import { Route, NavLink, Switch , Redirect} from 'react-router-dom';
 import './Blog.css';
 
+const AsyncNewPost = asyncComponent( () => {
+    return import('./NewPost/NewPost');
+});
+
 class Blog extends Component {
     state = {
-        auth: false
+        auth: true
     }
     
     render () {
@@ -37,10 +42,10 @@ class Blog extends Component {
                     <Route path="/" render={() => <h1> Home again </h1> }/>  */}
                     
                     <Switch>    
-                        {this.state.auth ? <Route path="/new-post" component={ NewPost } /> : null }
+                        {this.state.auth ? <Route path="/new-post" component={ AsyncNewPost } /> : null }
                         <Route path="/posts" component={Posts} />
-                        <Route render={() => <h1> Not Found </h1>} />
-                        {/* <Redirect from='/' to='/posts' />  */}
+                        {/* <Route render={() => <h1> Not Found </h1>} /> */}
+                        <Redirect from='/' to='/posts' /> 
                         {/* <Route path="/:id" exact component={FullPost} /> */}
                     </Switch>
 
