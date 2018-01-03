966de7d6fddaab5c26b3c8accead105b4e86f85d
deepaliprasad <deepaliprasad123@gmail.com>
2018-01-03 12:20:54 +0530

    conditional use of redirect

--- src/containers/Blog/NewPost/NewPost.js
+++ src/containers/Blog/NewPost/NewPost.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-
+import { Redirect } from 'react-router-dom';
 import './NewPost.css';
 
 class NewPost extends Component {
     state = {
         title: '',
         content: '',
-        author: 'Max'
+        author: 'Max',
+        submitted: false
     }
+
     componentDidMount() {
         console.log(this.props);
     }
@@ -21,14 +23,20 @@ class NewPost extends Component {
             axios.post('/posts', data)
                 .then( response => {
                     console.log(response);
+                    this.setState({submitted: true })
                 })
             
    }
     
 
     render () {
+        let redirected = null;
+        if ( this.state.submitted ){
+            redirected = <Redirect to="/posts" />
+        }
         return (
             <div className="NewPost">
+                { redirected }
                 <h1>Add a Post</h1>
                 <label>Title</label>
                 <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
